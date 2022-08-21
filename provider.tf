218d98133d819162c5fb72fe2b540cf663dfe945
ajreddy425 <82323282+ajreddy425@users.noreply.github.com>
2022-08-21 18:10:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,16 @@
-#   provider "aws"{
-#     region = "us-east-1"
+# #   provider "aws"{
+# #     region = "us-east-1"
+# #   }
+#  terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.27.0"
+#     }
 #   }
- terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.27.0"
-    }
-  }
+# }
+
+provider "aws" {
+  version = "~> 3.7.0"
+  region  = "us-east-1"
 }
